0d3f1ebfcbc3ae2015960485e14ba1acb3ebcb28
Owen Gage <owengage@gmail.com>
2018-01-12 17:49:23 +0000

    Get dev server working properly

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const path = require('path');
 module.exports = {
 	entry: './src/app.js',
 	output: {
-		path: __dirname + '/dist',
+		path: path.join(__dirname, '/dist'),
 		filename: 'bundle.js',
+		publicPath: 'dist',
 	},
+	devtool: "sourcemap",
 	module: {
 		loaders: [
 			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
