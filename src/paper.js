d455a1b2065f2109d566d0d8e2f6882fd574a02c
Owen Gage <owengage@gmail.com>
2018-01-17 19:34:09 +0000

    Press Ctrl-K to add links
    Doesn't actually populate the href attribute yet. But acts as a nice proof of
    concept. Need to work on some sort of generic modal so I can ask for input.

--- src/paper.js
+++ src/paper.js
@@ -80,17 +80,68 @@ function debugSelectionInfo(e) {
 
 export default class Paper {
     constructor(container) {
+        this.container = container;
+
         const hw = window.document.createTextNode('Hello, world!');
         container.appendChild(hw);
 
         container.setAttribute('contenteditable', true);
-        container.addEventListener('input', this.handleInput);
-        container.addEventListener('selectionchange', this.handleSelectionChange);
-        container.addEventListener('keydown', this.handleKeyDown);
+        container.addEventListener('input', this.handleInput.bind(this));
+        container.addEventListener('keydown', this.handleKeyDown.bind(this));
+        
+        // Selection change only fires on the document, not individual elements
+        // We'd have to filter out selection events not in our container.
+        //window.document.addEventListener('selectionchange', this.handleSelectionChange);
     }
 
     handleKeyDown(e) {
         console.log('key pressed');
+        const { key, ctrlKey, altKey, shiftKey, metaKey } = e
+        console.log({key, ctrlKey, altKey, shiftKey, metaKey});
+
+        // Create a link.
+        if (key === 'k' && ctrlKey) {
+            e.preventDefault();
+            const selection = window.getSelection();
+            const cursor = this.makeLink(selection);
+            selection.removeAllRanges();
+            selection.addRange(cursor);
+        }
+    }
+
+    // A selection that begins and ends in our container.
+    isInternalSelection(selection) {
+       return this.container.contains(selection.anchorNode)
+            && this.container.contains(selection.focusNode);
+    }
+
+    makeLink(selection) {
+        // Not handling links spanning more than one node for now.
+        if (this.isInternalSelection(selection) 
+            && selection.anchorNode.isSameNode(selection.focusNode)
+            && selection.anchorNode.nodeName === '#text') {
+
+            const { anchorNode, anchorOffset, focusNode, focusOffset } = selection;
+            const node = selection.anchorNode;
+            const left = Math.min(anchorOffset, focusOffset);
+            const right = Math.max(anchorOffset, focusOffset);
+
+            const linkText = node.splitText(left);
+            const trailingText = linkText.splitText(right - left);
+
+            const a = window.document.createElement('a');
+            a.setAttribute('href', '#');
+            a.appendChild(linkText);
+
+            this.container.insertBefore(a, trailingText); 
+         
+            // We want to return a new range that should be the new selection
+            // if the caller deems it should move the cursor.
+            const cursor = window.document.createRange();
+            cursor.setStart(trailingText, 0);
+            cursor.setEnd(trailingText, 0);
+            return cursor;
+        }
     }
 
     handleSelectionChange(e) {
